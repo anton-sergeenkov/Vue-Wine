037ea2bbf8dc22df3b8124a973928d243ed8988d
Anton <warnes23@yandex.ru>
2020-08-16 00:48:13 +0300

    starter

---
+++ js/index.js
@@ -0,0 +1,93 @@
+new Vue({
+    el: '#container',
+    data: {
+        wine: [
+            {
+                title: 'Чили',
+                name: 'Chile',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                    { img: '02.jpg', name: '', about: [] },
+                    { img: '03.jpg', name: '', about: [] },
+                    { img: '04.jpg', name: '', about: [] },
+                    { img: '05.jpg', name: '', about: [] },
+                ]
+            },
+            {
+                title: 'Грузия',
+                name: 'Georgia',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                    { img: '02.jpg', name: '', about: [] },
+                    { img: '03.jpg', name: '', about: [] },
+                    { img: '04.jpg', name: '', about: [] },
+                    { img: '05.jpg', name: '', about: [] },
+                    { img: '06.jpg', name: '', about: [] },
+                ]
+            },
+            {
+                title: 'Испания',
+                name: 'Spain',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                ]
+            },
+            {
+                title: 'Крым',
+                name: 'Crimea',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                    { img: '02.jpg', name: '', about: [] },
+                    { img: '03.jpg', name: '', about: [] },
+                ]
+            },
+
+
+            {
+                title: 'Болгария',
+                name: 'Bulgaria',
+                items: [
+                    {
+                        img: '01.jpg',
+                        name: '',
+                        about: ['Красное полусладкое', 'Дешевое и неплохое'],
+                    },
+                    {
+                        img: '02.jpg',
+                        name: '',
+                        about: ['Красное полусладкое', 'Дешевое и неплохое, не особо сладкое'],
+                    },
+                ]
+            },
+            {
+                title: 'Франция',
+                name: 'France',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                ]
+            },
+            {
+                title: 'Россия',
+                name: 'Russia',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                ]
+            },
+            {
+                title: 'Игристое',
+                name: 'Sparkling',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                    { img: '02.jpg', name: '', about: [] },
+                ]
+            },
+            {
+                title: 'Неизвестно',
+                name: 'Unknown',
+                items: [
+                    { img: '01.jpg', name: '', about: [] },
+                ]
+            },
+        ],
+    }
+});
