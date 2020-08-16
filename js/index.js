edfaede400c025a78f8c4249877fb97259dee73f
Anton <warnes23@yandex.ru>
2020-08-16 17:31:06 +0300

    z

--- js/index.js
+++ js/index.js
@@ -21,11 +21,14 @@ new Vue({
                 title: 'Чили',
                 name: 'Chile',
                 items: [
-                    { img: '03.jpg', name: 'Poyen Chardonnay',         type: 'Белое полусухое', about: [] },
-                    { img: '06.jpg', name: 'Poyen Chardonnay Reserva', type: 'Белое сухое',     about: [] },
+                    { img: '07.jpg', name: 'Sauvignon Blanc. Aves del Sur',         type: 'Белое сухое', grapes: 'Совиньон Блан', about: [] },
+                    { img: '05.jpg', name: 'Casillero del Diablo. Sauvignon Blanc', type: 'Белое сухое', grapes: 'Совиньон Блан', about: ['Не пробовал'] },
+
+                    { img: '03.jpg', name: 'Poyen Chardonnay',              type: 'Белое полусухое', grapes: '', about: [] },
+                    { img: '06.jpg', name: 'Poyen Chardonnay Reserva',      type: 'Белое сухое',     grapes: '', about: [] },
                     { img: '04.jpg', name: '', about: [] },
                     { img: '02.jpg', name: '', about: [] },
-                    { img: '05.jpg', name: '', about: [] },
+
                     { img: '01.jpg', name: '', about: [] },
                 ]
             },
@@ -33,13 +36,15 @@ new Vue({
                 title: 'Грузия',
                 name: 'Georgia',
                 items: [
-                    { img: '01.jpg', name: '', about: [] },
-                    { img: '02.jpg', name: '', about: [] },
-                    { img: '03.jpg', name: '', about: [] },
-                    { img: '04.jpg', name: '', about: [] },
-                    { img: '05.jpg', name: '', about: [] },
-                    { img: '06.jpg', name: '', about: [] },
-                    { img: '07.jpg', name: '', about: [] },
+                    { img: '01.jpg', name: 'Алазанская долина', type: 'Красное полусладкое', grapes: 'Саперави, Александроули, Оджалеши', about: [] },
+                    { img: '02.jpg', name: 'Ахашени',           type: 'Красное полусладкое', grapes: 'Саперави',                          about: [] },
+                    { img: '03.jpg', name: 'Киндзмараули',      type: 'Красное полусладкое', grapes: 'Саперави',                          about: [] },
+                    { img: '08.jpg', name: 'Хванчкара',         type: 'Красное полусладкое', grapes: 'Александроули, Муджуретули',        about: [] },
+                    { img: '07.jpg', name: '',                  type: '',                    grapes: '',                                  about: [] },
+
+                    { img: '04.jpg', name: '',                  type: '',                    grapes: '',                                  about: [] },
+                    { img: '05.jpg', name: '',                  type: '',                    grapes: '',                                  about: [] },
+                    { img: '06.jpg', name: 'Ркацители',         type: 'Белое сухое',         grapes: '',                                  about: [] },
                 ]
             },
             {
